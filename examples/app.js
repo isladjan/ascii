50fa6715932275b2dadfd51068f5dcae202ff772
isladjan <isladjan83@gmail.com>
2024-08-11 15:17:47 +0200

    Change Folder Structure

--- examples/app.js
+++ examples/app.js
@@ -1,7 +1,7 @@
 import { WebGLRenderer, Scene, PerspectiveCamera, Mesh, DirectionalLight, MeshStandardMaterial} from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { EffectComposer, RenderPass, EffectPass } from "postprocessing";
-import { ASCIIEffect } from './asciiEffect.min.js'
+import { ASCIIEffect } from '../asciiEffect.min.js'
 let touchDevice;
 
 
